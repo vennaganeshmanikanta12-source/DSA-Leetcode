a76ce3ad5b0078cc746c4fa4cd5b2d7f2fc0edf3
Venna Ganesh Manikanta <vennaganeshmanikanta12@gmail.com>
2026-09-25 20:53:32 +0530

    Add Easy Python Solution for 1. Two Sum from LeetCode

---
+++ LeetCode/Patterns/Hash Map & Hashing/1. Two Sum/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen={}
+        for i,num in enumerate(nums):
+            needed=target-num
+            if needed in seen:
+                return [i,seen[needed]]
+            seen[num]=i
+        return []
